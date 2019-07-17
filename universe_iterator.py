1581477288b967ac324dccf1a5478ae60acc3c9c
Maurycy Błaszczak <mgblaszczak@gmail.com>
2019-07-17 20:02:22 +0200

    formatting

--- universe_iterator.py
+++ universe_iterator.py
@@ -2,15 +2,21 @@
 
 """universe_iterator creates every possible black and white image.
 
-Everything is represented among these images - your facebook photo, the Avatar movie (as frames), the Bible, or even the picture of the very moment you read this description.
+Everything is represented among these images - your facebook photo, the Avatar
+movie (as frames), the Bible, or even the picture of the very moment you read
+this description.
 
-Using default values, there is about 2^1000 possible images. Some of them are just rotations of one another. Therefore, we can roughly estimate that there are about (2^1000) / 4 unique images.
+Using default values, there is about 2^1000 possible images. Some of them
+are just rotations of one another. Therefore, we can roughly estimate
+that there are about (2^1000) / 4 unique images.
 
 Example:
     $ python universe_iterator.py
 
 Attributes:
-    SIDE (int): Holds the image side length. There is only one constant holding the length of the side, therefore produced image can only be a square.
+    SIDE (int): Holds the image side length. There is only one constant
+        holding the length of the side, therefore produced image can
+        only be a square.
     SIZE (int): Holds the number of pixels in desired image.
     ORDER ():
     NUMBER_LIST ():
@@ -28,34 +34,41 @@ Todo:
 """
 
 import random
-import numpy as np
+
 from PIL import Image
+
+import numpy as np
 
 SIDE = 100
 SIZE = SIDE ** 2
 
 
 def dec_to_bin(dec_number: int) -> str:
-    """Transforms a dec number into a bin number.
+    """Transform a dec number into a bin number.
 
     Args:
         dec_number: Decimal number to be translated into binary one.
 
     Returns:
-        A binary number as a string. Before returning the value, transformed number is stripped form two zeros at the beggining.
+        A binary number as a string. Before returning the value, transformed
+        number is stripped form two zeros at the beggining.
 
     """
     return bin(dec_number)[2:]
 
 
 def create_digits_list(variation_number: int, size_of_image: int) -> np.array:
-    """Creates a digits list of given image variation.
+    """Create a digits list of an image variation.
 
-    This function takes the number of given variation of image (1, 2, 3, ...), translates it into binary, makes a list of digits of it, and fills the rest of the list with zeros up to the size of the image.
+    This function takes the number of given variation of image (1, 2, 3, ...),
+    translates it into binary, makes a list of digits of it, and fills
+    the rest of the list with zeros up to the size of the image.
 
     Args:
         variation_number: The ordered number of variation of an image.
-            E.g. images made of only 0 is the first variation; image made of 0 and 1 at the end is the second variation; image made of 0 and 10 at the end is the third variation.
+            E.g. images made of only 0 is the first variation;
+            image made of 0 and 1 at the end is the second variation;
+            image made of 0 and 10 at the end is the third variation.
         size_of_image: The number of pixels in the image.
 
     Returns:
@@ -73,7 +86,8 @@ def create_digits_list(variation_number: int, size_of_image: int) -> np.array:
 #  Assign colour black for 0 in the grid, and colour white for 1 in the grid.
 
 
-def number_to_color(number_grid: np.array, x_axis: int, y_axis: int) -> tuple(int):
+def number_to_color(number_grid: np.array, x_axis: int,
+                    y_axis: int) -> tuple(int):
     """
 
     Args:
